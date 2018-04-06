db6139ae1813eb4eded011706a02fedd3d709473
Arie Schouten <arie-schouten@outlook.com>
2018-04-06 12:21:28 +0200

    kiek m goan

--- ExamControl/Controllers/ExamController.cs
+++ ExamControl/Controllers/ExamController.cs
@@ -43,6 +43,37 @@ namespace ExamControl.Controllers
 
             return RedirectToAction("Index");
         }
+        [HttpPost]
+        public async Task<JsonResult> UploadHomeReport(string id)
+        {
+            try
+            {
+                foreach (string file in Request.Files)
+                {
+                    var fileContent = Request.Files[file];
+                    if (fileContent != null && fileContent.ContentLength > 0)
+                    {
+                        // get a stream
+                        var stream = fileContent.InputStream;
+                        // and optionally write the file to disk
+                        var fileName = Path.GetFileName(file);
+                        var path = Path.Combine(Server.MapPath("~/App_Data/Images"), fileName);
+                        using (var fileStream = File.Create(path))
+                        {
+                            stream.CopyTo(fileStream);
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return Json("Upload failed");
+            }
+
+            return Json("File uploaded successfully");
+        }
+
 
         [Authorize(Roles = "Admin")]
         public ActionResult InsertGrades()
